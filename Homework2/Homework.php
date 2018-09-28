55e9e73f2d562cf7c7a292e119da394fe31aca00
Andres Avila Brenes <alber1426@gmail.com>
2018-09-28 13:23:17 -0600

    Se modifico en el momento de imprimir la informacion debes de ir palabra por palabra que sea linea por linea

--- Homework2/Homework.php
+++ Homework2/Homework.php
@@ -1,12 +1,11 @@
 <?php
 
-
-
 $file = fopen("products.csv", "r");
 for ($i = 0; $row = fgetcsv($file ); ++$i) {
     for ($c=0; $c < count($row); $c++) {
-        echo $row[$c]."\n";
+        echo "$row[$c]\t";
     }
+    echo "\n";
 }
 fclose($file);
 
